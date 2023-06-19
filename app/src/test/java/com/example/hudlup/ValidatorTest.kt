b81847caab9b96be2b3b1cd83e2b210712c4e216
James Palfrey <james.palfrey@astraappivate.co.uk>
2023-06-19 10:14:49 +0100

    changed textValidator tests to Object.

--- app/src/test/java/com/example/hudlup/ValidatorTest.kt
+++ app/src/test/java/com/example/hudlup/ValidatorTest.kt
@@ -5,19 +5,19 @@ import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class ValidatorTest {
-    val textValidator= TextValidator()
+
     @Test
     fun testIfStringContainsSpecialCharacters(){
-        assertEquals(true,textValidator.hasSpecialCharacters("asdasd1234"))
-        assertEquals(true,textValidator.hasSpecialCharacters("@!*"))
-        assertEquals(false,textValidator.hasSpecialCharacters("James"))
+        assertEquals(true,TextValidator.hasSpecialCharacters("asdasd1234"))
+        assertEquals(true,TextValidator.hasSpecialCharacters("@!*"))
+        assertEquals(false,TextValidator.hasSpecialCharacters("James"))
     }
 
     @Test
     fun testIfEmailValidates(){
-        assertEquals(true, textValidator.isEmailAddress("abc@gmail.com"))
-        assertEquals(false, textValidator.isEmailAddress("abc@gmailcom"))
-        assertEquals(false, textValidator.isEmailAddress("abcgmail.com"))
-        assertEquals(false, textValidator.isEmailAddress("abcgmailcom"))
+        assertEquals(true, TextValidator.isEmailAddress("abc@gmail.com"))
+        assertEquals(false, TextValidator.isEmailAddress("abc@gmailcom"))
+        assertEquals(false, TextValidator.isEmailAddress("abcgmail.com"))
+        assertEquals(false, TextValidator.isEmailAddress("abcgmailcom"))
     }
 }
