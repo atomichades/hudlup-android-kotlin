cd32b88eec05bf437573134020427715d3765aac
James Palfrey <james.palfrey@astraappivate.co.uk>
2023-06-19 09:43:08 +0100

    added errors on textfields added error message for login page added signup button to SignUpFragment

--- app/src/main/java/com/example/hudlup/onboarding/SignUpFragment.kt
+++ app/src/main/java/com/example/hudlup/onboarding/SignUpFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import com.example.hudlup.R
 import com.example.hudlup.databinding.FragmentSignUpBinding
+import com.example.hudlup.util.TextValidator
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 private var _binding: FragmentSignUpBinding? = null
 private val binding get() = _binding!!
@@ -27,7 +32,7 @@ class SignUpFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        setupButtons()
+        setupOnClicks()
         _binding?.toolbar?.setNavigationOnClickListener {
             NavHostFragment.findNavController(this).popBackStack()
 
@@ -37,6 +42,42 @@ class SignUpFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun setupOnClicks(){
+        binding.signUpBtn.setOnClickListener {
+            if (checkTextFieldsAreFull()){
+                Firebase.auth.createUserWithEmailAndPassword(binding.emailEditTxt.toString(), binding.password2EditTxt.text.toString()).addOnCompleteListener {
+                    task -> {
+                        if (task.isSuccessful){
+                            //TODO: Move to next activity
+                        } else {
+                            //TODO: show error message
+                            binding.para1.setTextAppearance(R.style.error)
+                            binding.para1.setText(task.result.toString())
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun checkTextFieldsAreFull() : Boolean{
+        var result = false
+        if (binding.firstnameEditTxt.text.isEmpty()||TextValidator.hasSpecialCharacters(binding.firstnameEditTxt.text.toString())){
+            binding.firstnameEditTxt.setError("You must enter a valid firstname")
+        } else if (binding.lastnameEditTxt.text.isEmpty()||TextValidator.hasSpecialCharacters(binding.lastnameEditTxt.text.toString())){
+            binding.lastnameEditTxt.setError("You must enter a valid lastname")
+        } else if (binding.ageEditTxt.text.isEmpty()){
+            binding.ageEditTxt.setError("You must enter your age")
+        } else if (binding.emailEditTxt.text.isEmpty()){
+            binding.emailEditTxt.setError("You must enter your email address")
+        }else if (TextValidator.isEmailAddress(binding.emailEditTxt.text.toString())){
+            binding.emailEditTxt.setError("This does not look like a valid email")
+        }else {
+            result = true
+        }
+        return result
     }
 
     companion object TAG {
--- app/src/main/java/com/example/hudlup/util/TextValidator.kt
+++ app/src/main/java/com/example/hudlup/util/TextValidator.kt
@@ -1,12 +1,16 @@
 package com.example.hudlup.util
 
-class TextValidator {
+object TextValidator {
 
     fun hasSpecialCharacters (aString: String): Boolean {
         return aString.contains("[0-9!\"#$%&'()*+,-./:;\\\\<=>?@\\[\\]^_`{|}~]".toRegex())
     }
 
-    fun isEmailAddress (aString: String) :Boolean {
+    public fun isEmailAddress (aString: String) :Boolean {
         return aString.contains("[A-Za-z]+@[A-Za-z]+\\.[A-Za-z]+".toRegex())
+    }
+
+    fun signUpEditTextFiftyOrLess (aString:String): Boolean{
+        return aString.length < 25
     }
 }
